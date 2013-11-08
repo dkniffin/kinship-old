ea40a11a5a36befee8b4529ca2d54f56b6ccb2d9
Derek Kniffin <oddityoverseer13@gmail.com>
2013-11-08 14:49:04 -0700

    Added some CSS and templated out the home page a bit more

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,14 @@
 class StaticPagesController < ApplicationController
   def home
+  	@blurb_content = <<-eos
+  		<p>Site blurb will go here. Eventually, it'll come from the database</p>
+  		<br />
+  		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor 
+	    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+	    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
+	    irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+	    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+	    deserunt mollit anim id est laborum.</p>
+  	eos
   end
 end
