a68d9e8395794f3592b6a986d672da23a9237c00
Derek Kniffin <oddityoverseer13@gmail.com>
2013-11-08 12:51:29 -0700

    Reorganized home into a more general static_pages, and wrote a test

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Kinship::Application.routes.draw do
-  get "home/index"
+  get "static_pages/home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'home#index'
+  root 'static_pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
