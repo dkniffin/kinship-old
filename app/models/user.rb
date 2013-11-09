7d47855b6d49a66a3353e3803817bd45b688f592
oddityoverseer13 <oddityoverseer13@gmail.com>
2013-11-08 22:53:36 -0700

    Added some validation and tests for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+	validates :username, presence: true
+	VALID_EMAIL_REGEX = /\A[A-Z0-9._%+-]+@[A-Z0-9.\-]+\.[A-Z]{2,4}\z/i
+	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
 end
