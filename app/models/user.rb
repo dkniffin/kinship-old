646201f107b8719a2167d1f0d253f16fcc07affd
Gregory Wahl <wahlg@colorado.edu>
2013-11-10 21:32:38 -0800

    finished email uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  before_save { self.email = email.downcase }
 	validates :username, presence: true
 	VALID_EMAIL_REGEX = /\A[A-Z0-9._%+-]+@[A-Z0-9.\-]+\.[A-Z]{2,4}\z/i
 	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131109050433) do
+ActiveRecord::Schema.define(version: 20131111051949) do
 
   create_table "users", force: true do |t|
     t.string   "username"
@@ -19,5 +19,7 @@ ActiveRecord::Schema.define(version: 20131109050433) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
