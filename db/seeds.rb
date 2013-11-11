dc931394beadcde2a6502ceb2d43bf54898ae0ba
Derek Kniffin <oddityoverseer13@gmail.com>
2013-11-11 16:37:32 -0700

    Added an admin user to the DB seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(username: 'admin', email: 'nobody@none.com', password: 'kinship', password_confirmation: 'kinship')
