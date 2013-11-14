e159f381d8029ebd96bcf56232561cde92bb8f22
Derek Kniffin <oddityoverseer13@gmail.com>
2013-11-14 09:51:35 -0700

    Created the admin controller and views

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,16 @@
+class AdminController < ApplicationController
+	def home
+	end
+	
+	def look_and_feel
+	end
+
+	def privacy
+	end
+
+	def users
+	end
+
+	def logs
+	end
+end
---
+++ spec/views/admin/look_and_feel.html.erb_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe "admin/look_and_feel.html.erb" do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
