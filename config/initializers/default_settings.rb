db28881975da4457243bbda7bb218c9dc8e2abc0
Derek Kniffin <oddityoverseer13@gmail.com>
2013-11-18 15:41:12 -0700

    Maybe this will fix the heroku push

--- config/initializers/default_settings.rb
+++
@@ -1,3 +0,0 @@
-Setting.default[:homepage_blurb] = <<-eos
-<p>This is where the homepage blurb will go. It can be configured through the admin interface</p>
-eos
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,15 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+# Admin user
 User.create(username: 'admin', 
 			email: 'nobody@none.com', 
 			role: User::ROLE_ADMIN,
 			password: 'kinship', 
 			password_confirmation: 'kinship')
+
+# Default settings
+Setting.save_default(:homepage_blurb, <<-eos
+<p>This is where the homepage blurb will go. It can be configured through the admin interface</p>
+eos
+)
