8c06bc99d5c2515bd3b19b1240d1b1e2adfbaa31
Derek Kniffin <oddityoverseer13@gmail.com>
2013-11-18 14:40:00 -0700

    Testing

--- config/initializers/default_settings.rb
+++ config/initializers/default_settings.rb
@@ -1,4 +1,1 @@
-Setting.save_default(:homepage_blurb, <<-eos
-<p>This is where the homepage blurb will go. It can be configured through the admin interface</p>
-eos
-)
+Setting.save_default(:homepage_blurb, 'meh')
