d46782976058841b1db815fc07d6e6df614f5b52
Gregory Wahl <gregory.wahl@colorado.edu>
2013-11-18 11:12:24 -0700

    added default profile photo for person.  Made Name and gender mandatory

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,9 +1,13 @@
 class Person < ActiveRecord::Base
-	has_attached_file :photo, :styles => { :small => "150x150>" },
+	has_attached_file :photo, :styles => { :small => "256x256>" },
+		:default_url => "/assets/default_avatar.png",
 		:url  => "/assets/products/:id/:style/:basename.:extension",
 		:path => ":rails_root/public/assets/products/:id/:style/:basename.:extension"
 
-	validates_attachment_presence :photo
 	validates_attachment_size :photo, :less_than => 5.megabytes
 	validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png']
+	validates :first_name, :presence => true
+	validates :last_name, :presence => true
+	validates :gender, :presence => true
+        
 end
