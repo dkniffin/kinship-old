2dc08b7cff681ef051acbc3d9a124fdfc80fe48f
Derek Kniffin <kniffin@wheatley.colorado.edu>
2013-11-25 09:35:56 -0700

    Changed the geocoder to Nominatim

---
+++ config/initializers/geocoder.rb
@@ -0,0 +1,7 @@
+Geocoder.configure(
+	# geocoding service
+	:lookup => :nominatim
+	# TODO: We'll want to implement a delay somehow
+	# TODO: We'll want to include some admin contact info in the search params
+	# and/or headers
+)
