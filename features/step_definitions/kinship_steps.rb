d5a1e73c7696482fa266f2f89f5a89262fbd9cf9
Gregory Wahl <wahlg@colorado.edu>
2013-12-02 22:07:40 -0800

    Added one cucumber feature test

---
+++ features/step_definitions/kinship_steps.rb
@@ -0,0 +1,48 @@
+require 'uri'
+require 'cgi'
+
+module WithinHelpers
+  def with_scope(locator)
+    locator ? within(*selector_for(locator)) { yield } : yield
+  end
+end
+World(WithinHelpers)
+
+Given /^(?:|I )am on (.+)$/ do |page_name|
+  visit path_to(page_name)
+end
+
+When /^(?:|I )go to (.+)$/ do |page_name|
+  visit path_to(page_name)
+end
+
+When /^(?:|I )press "([^"]*)"$/ do |button|
+  click_button(button)
+end
+
+When /^(?:|I )follow "([^"]*)"$/ do |link|
+  click_link(link)
+end
+
+Then /^(?:|I )should see "([^"]*)"$/ do |text|
+  if page.respond_to? :should
+    page.should have_content(text)
+  else
+    assert page.has_content?(text)
+  end
+end
+###### BEGIN CUSTOM STEPS ##########################
+Given /the following people exist/ do |people_table|
+  people_table.hashes.each do |person|
+    if !Person.find_by_first_name_and_last_name(person[:first_name], person[:last_name]) then
+       Person.create!(person)
+    end
+  end
+end
+
+Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
+  #  ensure that that e1 occurs before e2.
+  #  page.body is the entire content of the page as a string.
+  page.body.should =~ /(\s|\S)+#{e1}(\s|\S)+#{e2}/
+end
+
