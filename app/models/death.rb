dbd6ed25ba49044cd44302e0dad5f3afeb7e2d63
Derek Kniffin <kniffin@wheatley.colorado.edu>
2013-12-04 11:09:04 -0700

    Fixed a bug where living people were required to be dead

--- app/models/death.rb
+++ app/models/death.rb
@@ -2,7 +2,7 @@ class Death < ActiveRecord::Base
 	belongs_to :person
 	before_validation :dead_if_date
 
-	validates :dead, inclusion: { in: [true] }, if: "date.nil?"
+	validates :dead, inclusion: { in: [true] }, if: "!date.nil?"
 
 
 	private
