f161e458149c531dbabeffdc976f9ed0240bdf1d
Derek Kniffin <kniffin@wheatley.colorado.edu>
2013-12-11 14:51:19 -0700

    Changed the login behaviour and the account dropdown

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,12 @@ class SessionsController < ApplicationController
 		user = User.find_by(username: params[:session][:username].downcase)
 		if user && user.authenticate(params[:session][:password])
 			sign_in user
-			redirect_to user
+			if user.person_id.nil?
+				redirect_to people_path
+			else
+				person = Person.find(user.person_id)
+				redirect_to person
+			end
 		else
 			flash.now[:error] = 'Invalid email/password combination'
 			render 'new'
