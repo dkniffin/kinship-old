04ef2941d7846565850da3831004f9d50c175b14
Gregory Wahl <gregory.wahl@colorado.edu>
2013-12-13 12:00:50 -0700

    Fixed bug in siblings

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -53,8 +53,12 @@ class PeopleController < ApplicationController
        @spouse = Person.find(@person.spouse_id)
     end
     
-    @primary_siblings = Person.where(:id => Birth.where(:father_id => @person.birth.father_id, 
+    if @person.birth.father_id != nil and @person.birth.mother_id != nil
+      @primary_siblings = Person.where(:id => Birth.where(:father_id => @person.birth.father_id, 
                              :mother_id => @person.birth.mother_id).map {|elt| elt.child_id}) - [@person]
+    else
+       @primary_siblings = []
+    end
     @step_siblings = Person.where(:id => (Birth.where(:father_id => @person.birth.father_id) -
                              Birth.where(:mother_id => @person.birth.mother_id)).map {|elt| elt.child_id})
     @json_pedigree_tree = get_json_pedigree_tree(@person)
