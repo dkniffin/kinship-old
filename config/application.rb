b196a0986f0ed6811cb1ac5f634dc997d85b9837
Derek Kniffin <derek.kniffin@gmail.com>
2015-12-07 08:12:19 -0500

    Hound feedback

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Kinship
   class Application < Rails::Application
     # Use the responders controller from the responders gem
     config.app_generators.scaffold_controller :responders_controller
-    config.responders.flash_keys = [ :success, :failure ]
+    config.responders.flash_keys = [:success, :failure]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
